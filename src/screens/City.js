af9b987393af79f3069ca595062c58927a3f5b1a
Brandon Newton <114821633+BengalPirate@users.noreply.github.com>
2024-01-07 14:41:00 -0600

    new city screen

--- src/screens/City.js
+++ src/screens/City.js
@@ -1,17 +1,41 @@
 import React from 'react'
-import { SafeAreaView, Text, StyleSheet } from 'react-native'
+import { SafeAreaView, Text, ImageBackground, StyleSheet, StatusBar } from 'react-native'
 
 const City = () => {
     return (
         <SafeAreaView style = {styles.container}>
-            <Text>City</Text>
+            <ImageBackground 
+                source = {require('../../assets/city-background.jpg')} 
+                styles={styles.imageLayout}
+            >
+                <Text style={styles.cityName}>London</Text>
+                <Text style={styles.countryName}>UK</Text>
+            </ImageBackground>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create ({
     container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0
+    },
+    imageLayout: {
         flex: 1
+    },
+    cityName: {
+        justifyContent: 'center',
+        alignSelf: 'center',
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    countryName: {
+        justifyContent: 'center',
+        alignSelf: 'center',
+        fontSize: 30, 
+        color: 'white',
+        fontWeight: 'bold'
     }
 })
 export default City
